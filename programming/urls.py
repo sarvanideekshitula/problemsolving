546cedded920192a6c6b28220333f061c4299965
sarvanideekshitula <sarvanimini@gmail.com>
2018-07-18 23:24:12 +0530

    Added templates

--- programming/urls.py
+++ programming/urls.py
@@ -3,10 +3,11 @@ from programming import views
 from programming.views import viewsummary, detailGroup
 
 urlpatterns = [
-    url(r'^$', views.addStudent, name='addstudent'),
+    url(r'^$', views.home, name='home'),
+    url(r'^addstudent/$', views.addStudent, name='addstudent'),
     url(r'^viewsummary/$', viewsummary.as_view(), name='viewsummary'),
     url(r'^creategroup/$', views.addgroup, name='addgroup'),
     url(r'^viewgroups/$', views.viewgroups, name='viewgroups'),
     url(r'^deletestudents/$', views.deleteStudents, name='deleteStudents'),
-    url(r'^(?P<slug>[\w-]+)/$', detailGroup.as_view(), name='detailgroup'),
+    url(r'^detailgroup/(?P<slug>[\w-]+)/$', detailGroup.as_view(), name='detailgroup'),
 ]
--- programming/views.py
+++ programming/views.py
@@ -8,6 +8,11 @@ from django.views.generic import ListView
 import re
 # Create your views here.
 
+def home(request):
+    temp = 'programming/home.html'
+    return render(request,temp,{})
+
+
 def addStudent(request):
     if request.method == 'POST':
         name = request.POST.get('name')
@@ -16,12 +21,9 @@ def addStudent(request):
         year = request.POST.get('year')
         u =  Student_details(name = name, codechef = codechef, codeforces = codeforces, year = year)
         u.save()
-        groupname = Groups.objects.get(groupname = "Bios").id
-        u.groupid.add(groupname)
-        u.save()
         stu = Student_details.objects.all()
         print stu
-        return HttpResponseRedirect('/programming/addStudent')
+        return HttpResponseRedirect('/programming/addstudent')
     return render(request, 'programming/addstudent.html',{})
 
 class viewsummary(ListView):
@@ -29,16 +31,27 @@ class viewsummary(ListView):
     template_name = 'programming/viewsummary.html'
 
 def addgroup(request):
-    if request.method == 'POST':
+     if request.method == 'POST':
         groupname = request.POST.get('name')
         students = request.POST.getlist('students[]')
-        u = Groups(groupname = groupname)
-        u.save()
-        for i in students:
-            stu = Student_details.objects.get(name = i)
-            stu.groupid.add(u.id)
-        return HttpResponseRedirect('/programming/addStudent')
-    return render(request, 'programming/creategroup.html',{'name':Student_details.objects.values_list('name',flat=True)})
+        groupnames = Groups.objects.values_list('groupname', flat=True)
+        for i in groupnames:
+            if i == groupname:
+                flag=0
+                break
+            else:
+                flag=1
+        if flag == 1:
+            u = Groups(groupname = groupname)
+            u.save()
+            for i in students:
+                stu = Student_details.objects.get(name = i)
+                stu.groupid.add(u.id)
+            return HttpResponseRedirect('/programming/addStudent')
+        else:
+            template = "programming/creategroup.html"
+            render(request, template)
+     return render(request, 'programming/creategroup.html',{'name':Student_details.objects.values_list('name', flat=True)})
 
 def viewgroups(request):
     if request.method == 'POST':
@@ -60,5 +73,7 @@ class detailGroup(ListView):
     model = Student_details
     template_name = 'programming/detailview.html'
 
-    def get_object(self, queryset=None):
-        return queryset.get(slug=self.slug)
+    def get_context_data(self, **kwargs):
+        context = super(detailGroup, self).get_context_data(**kwargs)
+        context['slug'] = self.kwargs['slug']
+        return context
